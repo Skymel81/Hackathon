e94d2fed128050e3490a37e3a715f678c9ce15c5
TheoSkymel <skymel81@gmail.com>
2025-05-29 16:46:03 +0200

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,39 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
+import GamePage from "./pages/GamePage.jsx";
+import Homepage from "./pages/Homepage.jsx";
+import NotFoundPage from "./pages/NotFoundPage.jsx";
+import themes from "./themes/themes.js";
+
+const router = createBrowserRouter([
+    {
+        element: <App />,
+        children: [
+            {
+                path: "/",
+                element: <Homepage />,
+            },
+            {
+                path: "gamepage/:theme",
+                element: <GamePage />,
+                loader: (route) => {
+                    const theme = route.params.theme;
+                    return themes[theme];
+                },
+            },
+            {
+                path: "*",
+                element: <NotFoundPage />,
+            },
+        ],
+    },
+]);
+
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>
+);
